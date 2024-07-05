fad929f154e17faf8f8d352b18a2e7d708e3f06f
key <keylinjaimes@gmail.com>
2024-07-05 09:53:55 +0200

    styles

--- src/main/java/com/keylin/WeCare/dto/FamilyDto.java
+++ src/main/java/com/keylin/WeCare/dto/FamilyDto.java
@@ -1,6 +1,5 @@
 package com.keylin.WeCare.dto;
 
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,19 +11,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class FamilyDto {
     private Long id;
-    @NotEmpty(message = "This field should not be empty")
     private String name;
-    @NotEmpty(message = "This field should not be empty")
     private String city;
-    @NotEmpty(message = "This field should not be empty")
     private String postalCode;
-    @NotEmpty(message = "This field should not be empty")
     private Double serviceFee;
-    @NotEmpty(message = "This field should not be empty")
     private String description;
-    @NotEmpty(message = "This field should not be empty")
     private int numChildren;
-    @NotEmpty(message = "This field should not be empty")
     private String photoUrl;
 
 }
--- src/main/java/com/keylin/WeCare/dto/NannyDto.java
+++ src/main/java/com/keylin/WeCare/dto/NannyDto.java
@@ -1,6 +1,5 @@
 package com.keylin.WeCare.dto;
 
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,20 +11,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class NannyDto {
 	private Long id;
-	@NotEmpty(message = "This field should not be empty")
 	private String name;
-	@NotEmpty(message = "This field should not be empty")
 	private int age;
-	@NotEmpty(message = "This field should not be empty")
 	private String city;
-	@NotEmpty(message = "This field should not be empty")
 	private String postalCode;
-	@NotEmpty(message = "This field should not be empty")
 	private Double serviceFee;
-	@NotEmpty(message = "This field should not be empty")
 	private String description;
-	@NotEmpty(message = "This field should not be empty")
 	private int yearsOfExperience;
-	@NotEmpty(message = "This field should not be empty")
 	private String photoUrl;
 }
